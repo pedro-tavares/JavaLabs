7f2c41b6abce3275852a147262e43293a447bc62
Pedro Alexandre Tavares - JavaLabs <patavares@outlook.com>
2020-10-18 14:26:57 +0200

    update Main classes

--- src/com/javalabs/collections/Main.java
+++ src/com/javalabs/collections/Main.java
@@ -6,7 +6,7 @@ package com.javalabs.collections;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World from JavaLabs!");
+		System.out.println("Hello World from JavaLabs - collections");
 		
 	}
 
---
+++ src/com/javalabs/introduction/Main.java
@@ -0,0 +1,13 @@
+package com.javalabs.introduction;
+
+/**
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from JavaLabs - introduction");
+		
+	}
+
+}
