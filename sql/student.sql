db70aa0929f7c342288b734e1c90329cb19f06d9
Pedro Alexandre Tavares - JavaLabs <patavares@outlook.com>
2020-10-22 20:25:37 +0200

    Update JDBC

---
+++ sql/student.sql
@@ -0,0 +1,9 @@
+USE javalabs;
+
+CREATE TABLE Student (
+    id int NOT NULL AUTO_INCREMENT,
+    firstName varchar(255) NOT NULL,
+    lastName varchar(255) NOT NULL,
+    Age int,
+    PRIMARY KEY (id)
+); 
